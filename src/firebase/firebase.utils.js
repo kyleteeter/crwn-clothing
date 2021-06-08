9f35aaceb8741383bd78fde25f2d30d4693174be
Kyle Teeter <kyleteeter@Kyles-MacBook-Pro-2.local>
2021-06-08 18:20:44 -0500

    Updated authentication

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,15 +4,42 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-        apiKey: "AIzaSyD1zBpI_AhDubvyoNi2cOyEf7_3913STnA",
-        authDomain: "crwn-db-205d3.firebaseapp.com",
-        projectId: "crwn-db-205d3",
-        storageBucket: "crwn-db-205d3.appspot.com",
-        messagingSenderId: "166528280061",
-        appId: "1:166528280061:web:7168a1c993ea35fa48a594"
+    apiKey: "AIzaSyD1zBpI_AhDubvyoNi2cOyEf7_3913STnA",
+    authDomain: "crwn-db-205d3.firebaseapp.com",
+    projectId: "crwn-db-205d3",
+    storageBucket: "crwn-db-205d3.appspot.com",
+    messagingSenderId: "166528280061",
+    appId: "1:166528280061:web:7168a1c993ea35fa48a594"
     };
 
     firebase.initializeApp(config);
+
+    export const createUserProfileDocument = async (userAuth, additionalData) => {
+        if (!userAuth) return;
+
+        const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+        const snapShot = await userRef.get();
+
+        if(!snapShot.exists){
+            const { displayName, email } = userAuth;
+            const createdAt = new Date();
+
+            try {
+                await userRef.set({
+                    displayName,
+                    email,
+                    createdAt,
+                    ...additionalData
+                })
+            } catch (error) {
+                console.log('error creating user', error.message);
+            }
+        }
+        console.log(userRef);
+        return userRef;
+    }
+
     
     export const auth = firebase.auth();
     export const firestore = firebase.firestore();
@@ -22,3 +49,5 @@ const config = {
     export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
     export default firebase;
+
+    
