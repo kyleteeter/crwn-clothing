b43fef02d623e9842c318f6c0228d5a4660ded09
Kyle Teeter <kyleteeter@Kyles-MacBook-Pro-2.local>
2021-06-10 18:39:31 -0500

    Update Sign in

--- src/App.js
+++ src/App.js
@@ -24,9 +24,15 @@ class App extends React.Component {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          console.log(snapShot);
-        })
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        });
       }
+      this.setState({ currentUser: userAuth });
     })
   }
 
